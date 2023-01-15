e56fec3ad4af65eaf65d9f36279327d39b77edcf
kwan_mohara <kwan@mohara.co>
2023-01-15 22:08:56 +0700

    fix deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,8 @@
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum update
 sudo yum upgrade
+sudo yum install epel-release
 sudo yum install nodejs npm -y
 
 # Clean working folder
-sudo find /home/ubuntu/test -type f -delete
+# sudo find /home/ubuntu/nodejs -type f -delete
